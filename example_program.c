5b5579e0a6ad0dfe10142e5e3edbe3b82c22757b
Eli <94979642+eli-miriam@users.noreply.github.com>
2024-12-04 10:21:09 +0000

    updating template program for fun and profit

--- example_program.c
+++ example_program.c
@@ -1,12 +1,18 @@
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#include <utils.h>
+
+#define INPUT_FILE "test.txt"
+#define SCALE_VARIABLE 255
+
 
 int main(void) {
-    FILE *input_file = fopen("test.txt", "r");
-    char line[255];
-    int left_list[6];
-    int right_list[6];
+    FILE *input_file = fopen(INPUT_FILE, "r");
+    int i = 0;
+    char line[SCALE_VARIABLE];
     while (fgets(line, 255, input_file)) {
         line[strcspn(line, "\n")] =0;
         printf("%s\n", line);
